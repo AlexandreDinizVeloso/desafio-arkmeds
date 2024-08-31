37492128eacf71f8cfc551b4d1483c4bdc994c30
AlexandreDinizVeloso <alexandredinizveloso@gmail.com>
2024-08-31 18:27:32 -0300

    add html render

--- arkmeds/equipamentos/views.py
+++ arkmeds/equipamentos/views.py
@@ -6,3 +6,6 @@ from .serializers import EquipamentoSerializer
 class EquipamentoViewSet(viewsets.ModelViewSet):
     queryset = Equipamento.objects.all()
     serializer_class = EquipamentoSerializer
+
+def index(request):
+    return render(request, 'equipamentos/index.html')
