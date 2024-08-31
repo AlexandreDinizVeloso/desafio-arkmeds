e40764c64bb8800408fb3c153f44a3e03b79c3f1
AlexandreDinizVeloso <alexandredinizveloso@gmail.com>
2024-08-31 14:56:10 -0300

    add equipamentos attributes

--- arkmeds/equipamentos/models.py
+++ arkmeds/equipamentos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Equipamento(models.Model):
+    tipo = models.CharField(blank=False, null=False,)
+    fabricante = models.CharField(blank=False, null=False)
+    modelo = models.CharField(blank=False, null=False)
+    numero_serie = models.CharField(blank=False, null=False)
+    data_compra = models.DateField()
+    valor_compra = models.DecimalField(decimal_places=2)
