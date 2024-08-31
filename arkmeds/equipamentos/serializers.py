8b9dc39c3dbbe3da870a903cf42b7d486bdbe569
AlexandreDinizVeloso <alexandredinizveloso@gmail.com>
2024-08-31 18:26:25 -0300

    change class values

--- arkmeds/equipamentos/serializers.py
+++ arkmeds/equipamentos/serializers.py
@@ -4,4 +4,8 @@ from .models import Equipamento
 class EquipamentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Equipamento
-        fields = "__all__"
+        fields = ['id', 'tipo', 'fabricante', 'modelo', 'numero_serie', 'data_compra', 'valor_compra']
+        extra_kwargs = {
+            'data_compra': {'required': False, 'allow_null': True},
+            'valor_compra': {'required': False, 'allow_null': True}
+        }
